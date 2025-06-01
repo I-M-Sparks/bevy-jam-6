1600cfe28725941641668e355bc71b26d71482bf
IMSparks <imsparks@pm.me>
2025-06-01 12:32:06 +0200

    adding assets and figured out how to render sprites

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Can't wait to jam!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, load_sprite)
+    .run();
+
+    //TODO Pre game start load all spritesheets
+    //TODO figure out how to read spritesheets
 }
+
+fn load_sprite(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+){
+    commands.spawn(Camera2d);
+
+    commands.spawn(Sprite::from_image(
+        asset_server.load("runes/PNG/Grey/Slab/runeGrey_slab_001.png"),
+    ));    
+}
+
+#[derive(Component)]
+struct Rune;
